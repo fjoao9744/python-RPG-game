77ea493fa44f1a9de4581622ba3497a62303af2f
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-01-17 16:33:06 -0300

    mudanças

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+from player import Player
+
 class World:
     def __init__(self):
         self.fase = 1
         
-    def __next__(self):
-        while self.fase < 100:
-            self.fase += 1
-            yield self.fase
+        
+    
         
         
 
--- player.py
+++ player.py
@@ -1,17 +1,19 @@
+from random import randint
+import pandas as pd
 
 class PlayerDefinition(type):
     def __new__(cls, name, bases, dct):
         
         def __str__(self):
-            return f"O objeto {name} é o jogador"
+            return f"object \"{name}\" is player"
         
         dct["__str__"] = __str__
         
         return super().__new__(cls, name, bases, dct)
 
 class Player(metaclass = PlayerDefinition):
-    def __init__(self, nome):
-        self.nome = nome
+    def __init__(self, name):
+        self.name = name
         self.level = 1
         
         self.__hp_max = 20
@@ -19,6 +21,18 @@ class Player(metaclass = PlayerDefinition):
         
         self.atk_min = 2
         self.atk_max = 6
+        
+        self.crit = 8
+        
+    def status(self):
+        status = {
+            "atributos" : ["name", "level", "hp max", "hp", "damage min", "damage max", "crit"],
+            "valores": [self.name, self.level, self.hp_max, self.__hp, self.atk_min, self.atk_max, self.crit]
+        }
+        
+        df = pd.DataFrame(status)
+        
+        print(df.to_string(index=False))
         
     # Decorador property para evitar um setter
     @property
@@ -29,7 +43,26 @@ class Player(metaclass = PlayerDefinition):
     def hp(self):
         return self.__hp
     
+    def damage(self):
+        damage = randint(self.atk_min, self.atk_max)
         
+        return damage
+    
+    def take_damage(self, damage):
+        self.__hp -= damage
+        if self.__hp < 0:
+            self.__hp = 0
         
+    def heal(self, hp):
+        self.__hp += hp
+        if self.__hp > self.__hp_max:
+            self.__hp = self.__hp_max
         
+    def atack(self, monster):
+        monster.take_damage(self.damage())
+
+
+
 joao = Player("joao")
+
+joao.status()
