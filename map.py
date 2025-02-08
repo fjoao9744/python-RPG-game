674114f5f817f797cb80d5691311d79dd9ab4f08
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-02-08 17:37:27 -0300

    map

--- map.py
+++ map.py
@@ -4,15 +4,39 @@ class Map:
     @staticmethod
     def map_generate():
         # ARG
-        monster_chance = random.random()
-        chest_chance = random.random() / 2
+        map = []
+        monster_chance_changeable = 1
+        chest_chance_changeable = 2
         
-        return [[[] for row in range(20)] for colunm in range(20)]
+        for colunms in range(20):
+            row = []
+            for rows in range(20):
+                monster_chance = random.random() / monster_chance_changeable
+                monster_chance_ = random.random()
+                                
+                if monster_chance >= monster_chance_: # if monster
+                    row.append("M")
+                    monster_chance_changeable += 1
+                    continue
+                
+                chest_chance = random.random() / chest_chance_changeable
+                chest_chance_ = random.random()
+                
+                if chest_chance >= chest_chance_: # if chest
+                    row.append("C")
+                    chest_chance_changeable += 1
+                    continue
+                
+                row.append([])
+            map.append(row)
+        
+        
+        return map
     
     matrix_map = map_generate()
     
     def __str__(self):
-        return "\n".join(str(row) for row in self.matrix_map)    
+        return "\n".join("".join(f"{str(cell):^3}" for cell in row) for row in self.matrix_map) 
 
 map = Map()
 
